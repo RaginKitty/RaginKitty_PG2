dcd1ea4401bfbb9f43847c0663fccbb529b5a43a
RaginKitty <38242725+RaginKitty@users.noreply.github.com>
2018-06-13 11:28:24 +0200

    Update GfxRectangle.cpp

--- Uebung_9/GfxRectangle.cpp
+++ Uebung_9/GfxRectangle.cpp
@@ -4,28 +4,26 @@ GfxRectangle::GfxRectangle(unsigned int xCoord, unsigned int yCoord, unsigned in
     GfxObject(xCoord, yCoord, heightNum), width(widthNum){}
 
 void GfxRectangle::helper(char table[50][50], char x){
-    if(coordY < 50 && coordX < 50 && (coordY + height) < 50 && (coordX + width) < 50){
-        for(unsigned int i = coordY; i < (coordY + height); ++i){
-            for(unsigned int j = coordX; j < (coordX + width); ++j){
-                if( x == '*')
-                    table[i][j] = '*';
-                else if(x == ' ')
-                    table[i][j] = ' ';
+    if(coordY < 50 && coordX < 50){
+        for(int i = coordY; (i < (coordY + height) && i < 50); ++i){
+            for(int j = coordX; (j < (coordX + width) && j < 50); ++j){
+                if(i >= 0 && j >= 0)
+                    table[i][j] = x;
             }
         }
     }
 }
 
 void GfxRectangle::draw(char table[50][50]){
-    std::cout << "Init drawing:" << std::endl;
+    //std::cout << "Init drawing:" << std::endl;
     GfxRectangle::helper(table, '*');
-    std::cout << "Drawing terminated!" << std::endl;
+    //std::cout << "Drawing terminated!" << std::endl;
 
 }
 
 void GfxRectangle::remove(char table[50][50]){
-    std::cout << "Init removing:" << std::endl;
+    //std::cout << "Init removing:" << std::endl;
     GfxRectangle::helper(table, ' ');
-    std::cout << "Removing terminated!" << std::endl;
+    //std::cout << "Removing terminated!" << std::endl;
 }
 
