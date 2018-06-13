0c3ce0f79bd704559d9931a06d8fec96a1f8a597
RaginKitty <38242725+RaginKitty@users.noreply.github.com>
2018-06-13 09:51:46 +0200

    Update GfxObject.h

--- Uebung_9/GfxObject.h
+++ Uebung_9/GfxObject.h
@@ -1,1 +1,23 @@
+#ifndef GFXOBJECT_H_INCLUDED
+#define GFXOBJECT_H_INCLUDED
+
+#include <iostream>
+
+class GfxObject{
+    protected:
+        unsigned int coordX;
+        unsigned int coordY;
+        unsigned int height;
+    public:
+        GfxObject();
+        GfxObject(unsigned int xCoord, unsigned int yCoord, unsigned int heightNum);
+        void draw(char table[50][50]);
+        void remove(char table[50][50]);
+        void move(int delta_x, int delta_y);
+};
+
+#include "GfxRectangle.h"
+#include "GfxTriangle.h"
+
+#endif // GFXOBJECT_H_INCLUDED
 
