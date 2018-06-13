7b69014275a34bc02ffa6cfa32cc4d847ea8511a
RaginKitty <38242725+RaginKitty@users.noreply.github.com>
2018-06-13 09:52:15 +0200

    Update GfxObject.cpp

--- Uebung_9/GfxObject.cpp
+++ Uebung_9/GfxObject.cpp
@@ -1,1 +1,13 @@
+#include "GfxObject.h"
+
+GfxObject::GfxObject():
+    coordX(0), coordY(0), height(0){}
+
+GfxObject::GfxObject(unsigned int xCoord, unsigned int yCoord, unsigned int heightNum):
+    coordX(xCoord), coordY(yCoord), height(heightNum){}
+
+void GfxObject::move(int delta_x, int delta_y){
+    coordX = coordX + delta_x;
+    coordY = coordY + delta_y;
+}
 
