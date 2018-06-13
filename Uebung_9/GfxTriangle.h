739e3d85e884b790565b09850189b0100fa5e1ae
RaginKitty <38242725+RaginKitty@users.noreply.github.com>
2018-06-13 09:50:27 +0200

    Update GfxTriangle.h

--- Uebung_9/GfxTriangle.h
+++ Uebung_9/GfxTriangle.h
@@ -1,1 +1,16 @@
+#ifndef GFXTRIANGLE_H_INCLUDED
+#define GFXTRIANGLE_H_INCLUDED
+
+#include "GfxObject.h"
+
+class GfxTriangle: public GfxObject{
+    public:
+        GfxTriangle(unsigned int xCoord, unsigned int yCoord, unsigned int heightNum);
+        void draw(char table[50][50]);
+        void remove(char table[50][50]);
+    private:
+        void helper(char table[50][50], char x);
+};
+
+#endif // GFXTRIANGLE_H_INCLUDED
 
