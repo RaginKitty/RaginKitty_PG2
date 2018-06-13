3f374b8152d2c2005bcec598b74c44670a9cb4b4
RaginKitty <38242725+RaginKitty@users.noreply.github.com>
2018-06-13 09:51:08 +0200

    Update GfxRectangle.h

--- Uebung_9/GfxRectangle.h
+++ Uebung_9/GfxRectangle.h
@@ -1,1 +1,17 @@
+#ifndef GFXRECTANGLE_H_INCLUDED
+#define GFXRECTANGLE_H_INCLUDED
+
+#include "GfxObject.h"
+
+class GfxRectangle: public GfxObject{
+    unsigned int width;
+    public:
+        GfxRectangle(unsigned int xCoord, unsigned int yCoord, unsigned int heightNum, unsigned int widthNum);
+        void draw(char table[50][50]);
+        void remove(char table[50][50]);
+    private:
+        void helper(char table[50][50], char x);
+};
+
+#endif // GFXRECTANGLE_H_INCLUDED
 
