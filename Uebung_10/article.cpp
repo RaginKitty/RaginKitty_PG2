aa953ca063739ad56873de5f14f4aaa71e1f678f
RaginKitty <38242725+RaginKitty@users.noreply.github.com>
2018-06-27 09:40:33 +0200

    Update article.cpp

--- Uebung_10/article.cpp
+++ Uebung_10/article.cpp
@@ -1,15 +1,73 @@
-#include "article.h"+
+#include "article.h"
 #include <iostream>
+#include <fstream>
 
-article::article():descr(0), id(0), stock(0), minstock(0), price(0){}
+article::article():id(0), stock(0), minstock(0), price(0), next(0){
+    descr = new char[20];
+}
 
 article::article(char* dscr, int ident, int stck, int minstck, float prce):
-        descr(dscr), id(ident), stock(stck), minstock(minstck), price(prce){}
+    descr(dscr), id(ident), stock(stck), minstock(minstck), price(prce), next(0){
+    descr = new char[20];
+    descr = dscr;
+    }
 
-void article::input(){}
-void article::printout(){}
-void article::savetofile(){}
-void article::loadfromfile(){}
+void article::input(){
+    std::cout << "Insert description of article: ";
+    std::cin >> descr;
+    std::cout << "Insert ID of article: ";
+    std::cin >> id;
+    std::cout << "Amount of items of this article in stock: ";
+    std::cin >> stock;
+    std::cout << "Insert minimum amount of items of this article in stock: ";
+    std::cin >> minstock;
+    std::cout << "Insert price of this article: ";
+    std::cin >> price;
+}
+void article::printout(){
+    std::cout << std::endl << "Description: " << descr << std::endl;
+    std::cout << "ID: " << id << std::endl;
+    std::cout << "Stock: " << stock << std::endl;
+    std::cout << "Minstock: " << minstock << std::endl;
+    std::cout << "Price: " << price << std::endl;
+}
+void article::savetofile(const char* fHandle){
+    std::ofstream fs(fHandle, std::ios::app);
+        if(!fs){
+            std::cout << "File couldn't be opened" << std::endl;
+        }
+        else{
+            fs << "***" << std::endl;
+            fs << descr << std::endl;
+            fs << id << std::endl;
+            fs << stock << std::endl;
+            fs << minstock << std::endl;
+            fs << price << std::endl;
+            fs.close();
+        }
+}
+
+void article::loadfromfile(const char* fHandle){
+    std::ifstream fs(fHandle);
+    char str[20];
+    if(!fs){
+        std::cout << "Couldn't open file!" << std::endl;
+    }
+    else{
+            char c;
+            while(c != '*'){
+                fs.seekg(1, std::ios::cur);
+                fs.get(c);
+            }
+            fs.seekg(3, std::ios::cur);
+            fs >> descr;
+            fs >> id;
+            fs >> stock;
+            fs >> minstock;
+            fs >> price;
+            fs.close();
+    }
+}
 void article::addstock(int delta){
     stock = stock + delta;
 }
@@ -23,36 +81,126 @@ void article::removestock(int delta){
 
 
 
-food::food(): article(), supplier(0), storeability(0){}
+food::food(): article(), storeability(0){
+    supplier = new char[20];
+}
 
 food::food(char* dscr, int ident, int stck, int minstck, float prce, char* suppler, int strability):
-        article(dscr, ident, stck, minstck, prce), supplier(suppler), storeability(strability){}
+    article(dscr, ident, stck, minstck, prce), storeability(strability){
+    supplier = new char[20];
+    supplier = suppler;
+    }
 
-void food::input(){}
-void food::printout(){}
-void food::savetofile(){}
-void food::loadfromfile(){}
+void food::input(){
+    article::input();
+    std::cout << "Insert name of supplier: ";
+    std::cin >> supplier;
+    std::cout << "Insert maximum number of articles to store: ";
+    std::cin >> storeability;
+    std::cout << std::endl;
+}
+void food::printout(){
+    article::printout();
+    std::cout << "Supplier: " << supplier << std::endl;
+    std::cout << "Storeability: " << storeability << std::endl;
+}
+void food::savetofile(const char* fHandle){
+    article::savetofile(fHandle);
+    std::ofstream fs(fHandle, std::ios::app);
+        if(!fs){
+            std::cout << "File couldn't be opened" << std::endl;
+        }
+        else{
+            fs << supplier << std::endl;
+            fs << storeability << std::endl;
+            fs.close();
+        }
+}
+void food::loadfromfile(const char* fHandle){
+    article::loadfromfile(fHandle);
+}
 
 
 
-book::book(): article(), publisher(0), author(0){}
+book::book(): article(){
+    publisher = new char[20];
+    author = new char[20];
+}
 
 book::book(char* dscr, int ident, int stck, int minstck, float prce, char* pblisher, char* authr):
-        article(dscr, ident, stck, minstck, prce), publisher(pblisher), author(authr){}
+    article(dscr, ident, stck, minstck, prce){
+    publisher = new char[20];
+    publisher = pblisher;
+    author = new char[20];
+    author = authr;
+    }
 
-void book::input(){}
-void book::printout(){}
-void book::savetofile(){}
-void book::loadfromfile(){}
+void book::input(){
+    article::input();
+    std::cout << "Insert name of publisher: ";
+    std::cin >> publisher;
+    std::cout << "Insert name of author: ";
+    std::cin >> author;
+    std::cout << std::endl;
+}
+void book::printout(){
+    article::printout();
+    std::cout << "Publisher: " << publisher << std::endl;
+    std::cout << "Author: " << author << std::endl;
+}
+void book::savetofile(const char* fHandle){
+    article::savetofile(fHandle);
+    std::ofstream fs(fHandle, std::ios::app);
+        if(!fs){
+            std::cout << "File couldn't be opened" << std::endl;
+        }
+        else{
+            fs << publisher << std::endl;
+            fs << author << std::endl;
+            fs.close();
+        }
+}
+void book::loadfromfile(const char* fHandle){
+    article::loadfromfile(fHandle);
+}
 
 
 
-material::material(): article(), supplier(0), weight(0){}
+material::material(): article(), weight(0){
+    supplier = new char[20];
+}
 
 material::material(char* dscr, int ident, int stck, int minstck, float prce, char* suppler, float wght):
-        article(dscr, ident, stck, minstck, prce), supplier(suppler), weight(wght){}
+    article(dscr, ident, stck, minstck, prce), weight(wght){
+    supplier = new char[20];
+    supplier = suppler;
+    }
 
-void material::input(){}
-void material::printout(){}
-void material::savetofile(){}
-void material::loadfromfile(){}
+void material::input(){
+    article::input();
+    std::cout << "Insert name of supplier: ";
+    std::cin >> supplier;
+    std::cout << "Insert weight of article: ";
+    std::cin >> weight;
+    std::cout << std::endl;
+}
+void material::printout(){
+    article::printout();
+    std::cout << "Supplier: " << supplier << std::endl;
+    std::cout << "Weight: " << weight << std::endl;
+}
+void material::savetofile(const char* fHandle){
+    article::savetofile(fHandle);
+    std::ofstream fs(fHandle, std::ios::app);
+        if(!fs){
+            std::cout << "File couldn't be opened" << std::endl;
+        }
+        else{
+            fs << supplier << std::endl;
+            fs << weight << std::endl;
+            fs.close();
+        }
+}
+void material::loadfromfile(const char* fHandle){
+    article::loadfromfile(fHandle);
+}
