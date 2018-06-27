fc861e75c48f795cef66606aa924c64d9f7707bf
RaginKitty <38242725+RaginKitty@users.noreply.github.com>
2018-06-27 11:36:00 +0200

    Update list.cpp

--- Uebung_10/list.cpp
+++ Uebung_10/list.cpp
@@ -13,15 +13,26 @@ void List::printout(){
         pointer = pointer->next;
     }
 }
-void List::addelement(){}
+void List::addelement(){
+    article* particle = first;
+    while(particle != 0){
+        particle = particle->next;
+        //particle = new article;
+    }
+}
 void List::loadfromfile(const char* fHandle){
     article* particle = first;
     std::ifstream fs(fHandle);
+    char temp[100];
     if(fs){
-        while(particle != 0){
+        std::cout << "File open!" << std::endl;
+        while(temp << fs.get()){
             particle->loadfromfile(fHandle);
             particle = particle->next;
         }
+    }
+    else{
+        std::cout << "File couldn't be opened!" << std::endl;
     }
 }
 void List::savetofile(const char* fHandle){
@@ -45,13 +56,19 @@ void FoodList::addelement(){}
 void FoodList::loadfromfile(const char* fHandle){
     food* fparticle = new food;
     article* particle = (article*)fparticle;
-    particle = first;
+    first = particle;
     std::ifstream fs(fHandle);
     if(fs){
+        std::cout << "Food File open!" << std::endl;
         while(particle != 0){
+            std::cout << "Food File while Schleife!" << std::endl;
             particle->loadfromfile(fHandle);
             particle = particle->next;
         }
+        std::cout << "nicht Food File while Schleife!" << std::endl;
+    }
+    else{
+        std::cout << "Food File couldn't be opened!" << std::endl;
     }
 }
 
@@ -64,10 +81,14 @@ void BookList::loadfromfile(const char* fHandle){
     particle = first;
     std::ifstream fs(fHandle);
     if(fs){
+        std::cout << "Book File open!" << std::endl;
         while(particle != 0){
             particle->loadfromfile(fHandle);
             particle = particle->next;
         }
+    }
+    else{
+        std::cout << "Book File couldn't be opened!" << std::endl;
     }
 }
 void BookList::searchauthor(){}
@@ -81,9 +102,13 @@ void MaterialList::loadfromfile(const char* fHandle){
     particle = first;
     std::ifstream fs(fHandle);
     if(fs){
+        std::cout << "Material File open!" << std::endl;
         while(particle != 0){
             particle->loadfromfile(fHandle);
             particle = particle->next;
         }
+    }
+    else{
+        std::cout << "Material File couldn't be opened!" << std::endl;
     }
 }
